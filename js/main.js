31a6e469db6de4a96d1861e7b255c290da43a8b8
Sarel <Sarelk@gmail.com>
2019-02-16 21:07:04 +0200

    progress

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ function init() {
 }
 
 // loads gallery 
-function renderGallery(filter=null) {
-    var imgs  = getImgsToDisplay(filter) || [];
+function renderGallery(filter = null) {
+    var imgs = getImgsToDisplay(filter) || [];
     console.log(imgs);
     var strHtmls = imgs.map(function (img) {
-    return `
+        return `
     <div class="lab_item">
     <div class="hexagon hexagon2">
         <div class="hexagon-in1">
@@ -19,9 +19,9 @@ function renderGallery(filter=null) {
         </div>
     </div>
 </div>
-    `  
+    `
     });
-document.querySelector('.imgs-container').innerHTML=strHtmls.join('');
+    document.querySelector('.imgs-container').innerHTML = strHtmls.join('');
 }
 
 // When user chose a photo - render it to editor
@@ -46,14 +46,14 @@ function renderCanvas(img) {
 }
 
 // Draw line 
-function drawFirstLine(colorChoosen, font, firstLine, fontSize) {
+function drawFirstLine(colorChoosen, font, firstLine, fontSize, yPos) {
     gCtx.font = fontSize + 'px ' + font;
     gCtx.strokeStyle = 'black';
     gCtx.lineWidth = 6;
     gCtx.textAlign = 'center';
-    gCtx.strokeText(firstLine, gCanvas.width / 2, 85);
+    gCtx.strokeText(firstLine, gCanvas.width / 2, yPos);
     gCtx.fillStyle = colorChoosen;
-    gCtx.fillText(firstLine, gCanvas.width / 2, 85);
+    gCtx.fillText(firstLine, gCanvas.width / 2, yPos);
 }
 
 // Draw line
@@ -89,13 +89,21 @@ function handleImageFromInput(ev, onImageReady) {
     reader.readAsDataURL(ev.target.files[0]);
 }
 
-function onDoMeme() {
-    var colorChoosen = document.querySelector('.txt-color').value;
-    var font = document.querySelector('.font').value;
-    var secondLine = document.querySelector('.second-line').value;
-    var fontSize = document.querySelector(".fontSize").value;
+function onDoMeme(lineWrapperClass) {
+    //first/second line ?
+    var wrapper = document.querySelector(lineWrapperClass);
 
-    gMeme.txts = createTxt(secondLine,fontSize,'center',colorChoosen,gCanvas.width / 2,85)
+
+    var colorChoosen = wrapper.querySelector('.txt-color').value;
+    var font = wrapper.querySelector('.font').value;
+    var lineInputValue = wrapper.querySelector('.line-input').value;
+    var fontSize = wrapper.querySelector(".fontSize").value;
+    var yPos = wrapper.querySelector('.yPos').value
+    if (lineWrapperClass == '.second-line-wrapper') {
+        yPos = gCanvas.height + (+yPos);
+    }
+    console.log(gCanvas.height, yPos);
+    gMeme.txts = createTxt(lineInputValue, fontSize, 'center', colorChoosen, gCanvas.width / 2, yPos)
     var img = new Image();
     img.src = 'img/' + gMeme.selectedImgId + '.jpg';
 
@@ -103,9 +111,9 @@ function onDoMeme() {
         gCanvas.width = img.naturalWidth
         gCanvas.height = img.naturalHeight
         gCtx.drawImage(img, 0, 0);
-        drawFirstLine(colorChoosen, font, secondLine, fontSize)
+        drawFirstLine(colorChoosen, font, lineInputValue, fontSize, yPos)
     }
-    
+
 
     // if (anotherLine) {
 
